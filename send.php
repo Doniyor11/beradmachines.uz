93ccce3528128a17310e246f45e9e6ec8b656d27
Doniyor Usomonov <doniyorbek.000@gmail.com>
2020-11-04 19:44:31 +0500

    index full

---
+++ send.php
@@ -0,0 +1,28 @@
+<?php
+
+/* https://api.telegram.org/bot1329215607:AAHt60dYbxOoTHc30V0-O5wUEFmjosX_e44/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+$name = $_POST['user_name'];
+$phone = $_POST['user_phone'];
+$email = $_POST['user_email'];
+$token = "1329215607:AAHt60dYbxOoTHc30V0-O5wUEFmjosX_e44";
+$chat_id = "-1001451645359";
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телефон: ' => $phone,
+  'Email' => $email
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram) {
+  header('Location: index.html');
+} else {
+  echo "Error";
+}
+?>
